8155511794cda45627b564bbfe5ba748f84ee53f
shalom.ango <shalom.ango@epitech.eu>
2021-09-27 14:44:08 +0100

    php mailer

--- sendemail.php
+++ sendemail.php
@@ -3,7 +3,7 @@ $name       = @trim(stripslashes($_POST['name']));
 $from       = @trim(stripslashes($_POST['email'])); 
 $subject    = @trim(stripslashes($_POST['subject'])); 
 $message    = @trim(stripslashes($_POST['message'])); 
-$to   		= 'angoshalom@gmail.com';//replace with your email
+$to   		= 'shalom.ango@epitech.eu';//replace with your email
 
 $headers   = array();
 $headers[] = "MIME-Version: 1.0";
